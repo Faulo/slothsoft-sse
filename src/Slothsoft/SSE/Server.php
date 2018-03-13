f23ba45ec6258cdee5307fffba6c158e5493f17e
Faulo <FauloLio@gmail.com>
2018-03-13 15:58:23 +0100

    declare(strict_types = 1);

--- src/Slothsoft/SSE/Server.php
+++ src/Slothsoft/SSE/Server.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types = 1);
 /***********************************************************************
  * ServerSentEvents v1.00 07.05.2014 © Daniel Schulz
  * 
--- src/Slothsoft/SSE/UnisonServer.php
+++ src/Slothsoft/SSE/UnisonServer.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types = 1);
 /***********************************************************************
  * \SSE\UnisonServer v1.00 29.05.2014 © Daniel Schulz
  * 
