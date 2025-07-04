de4ec27683fa219270639a171d9a5d9237719f37
Daniel Schulz <faulolio@gmail.com>
2025-07-04 20:49:22 +0200

    fix: version

--- src/registerModule.php
+++ src/registerModule.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types = 1);
+namespace Slothsoft\SSE;
 
 use Slothsoft\Farah\Module\Module;
-
 Module::registerWithXmlManifestAndDefaultAssets('slothsoft@sse', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'assets');
 
